742d3602708b251849b067138d3a7dd2f350e691
imperfectcastle <bart.sakowski@gmail.com>
2020-05-18 18:33:17 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -19,21 +19,7 @@ let toyCollection = document.querySelector('#toy-collection')
 function fetchToys() {
   return fetch('http://localhost:3000/toys')
     .then(resp => resp.json())
-    .then(toys => renderToys(toys))
-}
-
-function renderToys(toys) {
-  toys.forEach(function(toy){
-    
-    toyCollection.innerHTML +=
-     `<div class="card">
-      <h2> ${toy.name}</h2>
-      <img src=${toy.image} class="toy-avatar" />
-      <p> ${toy.likes} Like(s) </p>
-      <button class="like-btn">Like <3</button>
-      </div>`
-
-  })
+    .then(toys => toys.forEach(renderSingleToy))
 }
 
 function newToyListener() {
@@ -56,26 +42,62 @@ function newToyListener() {
      }
      fetch('http://localhost:3000/toys', configObj)
      .then(resp => resp.json())
-     .then(toy => {
-       const newToy = renderSingleToy(toy)
-       toyCollection.innerHTML += newToy
-     })
+     .then(toy => renderSingleToy(toy)
+     )
   })
 
 }
 
-
 function renderSingleToy(toy) {
-  return (  `
-  <div class="card">
+  toyCollection.innerHTML +=  `
+  <div class="card" data-id=${toy.id}>
     <h2> ${toy.name}</h2>
       <img src=${toy.image} class="toy-avatar" />
-      <p> ${toy.likes} Like(s) </p>
+      <p>${toy.likes} Like(s)</p>
       <button class="like-btn">Like <3</button>
   </div>`
-  )
 }
 
+const toyCard = document.querySelector('#card')
+
+function addLikes() {
+
+  toyCollection.addEventListener('click', function(event) {
+    
+    if (event.target.className === 'like-btn'){
+
+      increaseLike(event)
+      
+    }
+  })
+}
+
+function increaseLike(event) {
+  const likes = event.target.previousElementSibling
+  let numLikes = parseInt(likes.innerHTML)+1
+
+  const configObj = {
+    method: 'PATCH',
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      "likes": numLikes
+    })
+  }
+  console.log('hello') 
+  console.log(event.target)
+  const id = event.target.parentNode.dataset.id
+  fetch(`http://localhost:3000/toys/${id}`, configObj)
+  
+  .then(resp => resp.json())
+  .then(likeNum => { 
+    likes.innerHTML = `${numLikes} Like(s)`
+
+  })
+}
 
 fetchToys()
 newToyListener()
+addLikes()
