4af7df36f191180297cf51833ae6c1eaa32ef784
isaacmeedinaa <isaac.meedinaa@gmail.com>
2020-05-21 18:16:31 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 let addToy = false;
-let toyCollectionDiv = document.getElementById('toy-collection') 
+let toyCollectionDiv = document.getElementById('toy-collection')
 
-function main() {
-  getToys()
-  newToyListener()
-  likeToyListener()
+const main = () => {
+  fetchToys()
+  createToy()
+  likeToy()
+  deleteToy()
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -22,112 +23,153 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-function toyContent(toy) {
-  let toyCardDiv = document.createElement('div')
-  toyCardDiv.id = toy['id']
-  toyCardDiv.likes = toy['likes']
-  toyCardDiv.likeText = `${toyCardDiv.likes} Likes`
-  toyCardDiv.setAttribute('class', 'card')
-  toyCollectionDiv.appendChild(toyCardDiv)
+const renderToy = (toy) => {
+  // Creation of Toy Card
+  let toyCard = document.createElement('div')
+  toyCard.className = 'card'
 
+  // Creation of Toy Name
   let toyName = document.createElement('h2')
   toyName.innerText = toy['name']
+  toyCard.appendChild(toyName)
 
+  // Creation of Toy Image
   let toyImg = document.createElement('img')
-  toyImg.setAttribute('class', 'toy-avatar')
   toyImg.src = toy['image']
+  toyImg.className = 'toy-avatar'
+  toyCard.appendChild(toyImg)
 
+  // Creation of Toy Likes
   let toyLikes = document.createElement('p')
-  toyLikes.setAttribute('id', 'like-text')
-  toyLikes.innerText = toyCardDiv.likeText
+  toyLikes.innerText = `${toy['likes']} Likes`
+  toyCard.appendChild(toyLikes)
 
+  // Creation of Toy Like Button
   let toyLikeBtn = document.createElement('button')
-  toyLikeBtn.setAttribute('class', 'like-btn')
+  toyLikeBtn.dataset.id = toy['id']
+  toyLikeBtn.className = 'like-btn'
   toyLikeBtn.innerText = 'Like <3'
+  toyCard.appendChild(toyLikeBtn)
 
-  toyCardDiv.appendChild(toyName)
-  toyCardDiv.appendChild(toyImg)
-  toyCardDiv.appendChild(toyLikes)
-  toyCardDiv.appendChild(toyLikeBtn)
+  // Creation of Toy Delete Button
+  let toyDeleteBtn = document.createElement('button')
+  toyDeleteBtn.dataset.id = toy['id']
+  toyDeleteBtn.className = 'delete-btn'
+  toyDeleteBtn.innerText = 'Delete :('
+  toyCard.appendChild(toyDeleteBtn)
+
+  toyCollectionDiv.appendChild(toyCard)
 }
 
-function getToys() {
-  const toysUrl = 'http://localhost:3000/toys'
+const fetchToys = () => {
+  const URL = 'http://localhost:3000/toys'
 
-  fetch(toysUrl)
+  fetch(URL)
     .then(response => {
       return response.json()
     })
     .then(toys => {
-      for (const toy of toys) {
-        toyContent(toy)
-      }
+      toys.forEach(toy => {
+        renderToy(toy)
+      });
     })
 }
 
-function newToy(event) {
-  const formData = {
-    name: event.target[0].value,
-    image: event.target[1].value,
-    likes: 0
-  }
+const createToy = () => {
+  let createToyForm = document.getElementById('addToyForm')
 
-  const configObj = {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify(formData)
-  }
+  createToyForm.addEventListener('submit', (event) => {
+    event.preventDefault()
 
-  fetch(toysUrl, configObj)
-    .then(response => {
-      return response.json()
-    })
-    .then(toys => {
-    })
+    const URL = 'http://localhost:3000/toys'
+    
+    const formData ={
+      name: event.target[0].value,
+      image: event.target[1].value,
+      likes: 0
+    }
+
+    const configObj = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accepts': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    }
+
+    fetch(URL, configObj)
+      .then(response => {
+        return response.json()
+      })
+      .then(toy => {
+        renderToy(toy)
+      })
+
+    createToyForm.reset()
+  })
 }
 
-function newToyListener() {
-  let newToyForm = document.getElementById('addToyForm')
-
-  newToyForm.addEventListener('submit', function(event) {
-    newToy(event)
-  })  
-}
-
-function likeToy(toyCard) {
-  const toysUrlId = `http://localhost:3000/toys/${toyCard.id}`
-
-  const submitData = {
-    likes: toyCard.likes
-  }
-
-  const configObj = {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    },
-    body: JSON.stringify(submitData)
-  }
-
-  fetch(toysUrlId, configObj)
-    .then(response => {
-      return response.json()
-    })
-    .then(toys => {
-      toyCard.querySelector('p').innerText = `${toyCard.likes} Likes`
-    })
-}
-
-function likeToyListener() {
-  document.addEventListener('click', function(event) {
+const likeToy = () => {
+  document.addEventListener('click', (event) => {
     if (event.target.className === 'like-btn') {
-      let toyCard = event.target.parentNode
-      toyCard.likes += 1
-      likeToy(toyCard)
+      let likeBtn = event.target
+      let toyId = likeBtn.dataset.id
+
+      const updateURL = `http://localhost:3000/toys/${toyId}`
+
+      let likeCountText = likeBtn.previousSibling
+      let likeCount = parseInt(likeCountText.innerText.split(' ')[0])
+
+      const formData = {
+        likes: `${likeCount + 1}`
+      }
+
+      const configObj = {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accepts': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      }
+
+      fetch(updateURL, configObj)
+        .then(response => {
+          return response.json()
+        })
+        .then(toy => {
+          likeCountText.innerText = `${toy['likes']} Likes`
+        })
+    }
+  })
+}
+
+const deleteToy = () => {
+  document.addEventListener('click', (event) => {
+    if (event.target.className === 'delete-btn') {
+      let deleteBtn = event.target
+      let toyId = deleteBtn.dataset.id
+
+      let toyCard = deleteBtn.parentElement
+
+      const deleteURL = `http://localhost:3000/toys/${toyId}`
+
+      const configObj = {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accepts': 'application/json'
+        }
+      }
+
+      fetch(deleteURL, configObj)
+        .then(response => {
+          return response.json()
+        })
+        .then(toy => {
+          toyCard.remove()
+        })
     }
   })
 }
